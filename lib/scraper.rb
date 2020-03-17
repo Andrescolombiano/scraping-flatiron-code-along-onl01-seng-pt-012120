58debc37f7b9f359d0f9f8c6edec16389a980f15
Andres Carvajal <jc1993box@hotmail.com>
2020-03-17 21:38:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,6 +18,8 @@ class Scraper
   def make_courses
     self.gets_courses.each do |post|
       course = Course.new
+      course.title = post.css ("h2").text
+      course.schedule = post.css (".date".text)
       
     end 
   end
