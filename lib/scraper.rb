7b1c9a5893428767d04980da1c24c22500e2a835
Andres Carvajal <jc1993box@hotmail.com>
2020-03-17 21:28:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,15 +5,20 @@ require 'pry'
 require_relative './course.rb'
 
 
-
-
 class Scraper
+  
   def get_page
   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
   def get_courses
     self.get_page.css(".post")
+  end
+  
+  def make_courses
+    self.gets_courses.each do |post|
+      course = Course.new
+      
   end
   
 def print_courses
